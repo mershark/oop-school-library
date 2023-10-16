4aac18823f764d9d26aaf377d8f489d96e98b345
mershark <mersharktakyi@gmail.com>
2023-10-16 12:28:45 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,40 +1,15 @@
 require_relative 'app'
+require_relative 'menu'
 
 puts "\nWelcome to School Library App!\n\n"
-
-MENU = "Please choose an option by entering a number:
-1 - List all books
-2 - List all people
-3 - Create a person
-4 - Create a book
-5 - Create a rental
-6 - List all rentals for a given person ID
-7 - Exit".freeze
-
-def switch(get, app)
-  case get
-  when 1
-    app.list_books
-  when 2
-    app.list_people
-  when 3
-    app.create_person
-  when 4
-    app.create_book
-  when 5
-    app.create_rental
-  when 6
-    app.list_rentals
-  end
-end
 
 def main
   app = LibraryApp.new
   get = 0
-  while get != 7 and get < 7
-    puts MENU
+  until get == 7
+    Menu.display
     get = gets.chomp.to_i
-    switch(get, app)
+    app.execute(get)
   end
   puts 'Thank you for using this app!'
 end
--- menu.rb
+++ menu.rb
@@ -1,14 +1,14 @@
 class Menu
-    MENU = "Please choose an option by entering a number:
-    1 - List all books
-    2 - List all people
-    3 - Create a person
-    4 - Create a book
-    5 - Create a rental
-    6 - List all rentals for a given person ID
-    7 - Exit".freeze
-  
-    def self.display
-      puts MENU
-    end
+  MENU = "Please choose an option by entering a number:
+  1 - List all books
+  2 - List all people
+  3 - Create a person
+  4 - Create a book
+  5 - Create a rental
+  6 - List all rentals for a given person ID
+  7 - Exit".freeze
+
+  def self.display
+    puts MENU
   end
+end
